18df02733ba38d62ec855e8a60edd6179be90661
unknown <joachim_urquhart@hotmail.co.uk>
2020-05-27 19:39:50 +0200

    POC multiselect per group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {filter} from 'rxjs/operators';
 
 interface Pokemon {
   parentId: string
@@ -16,8 +18,92 @@ interface PokemonGroup {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'untitled1';
+export class AppComponent implements OnInit {
+  searchControl = new FormControl('');
+  groupControl = new FormGroup({});
+  intermediate: any = {};
+  selectControl = new FormControl();
+  pokemonGroups: PokemonGroup[] = [
+    {
+      id: 'grass',
+      name: 'Grass',
+      pokemon: [
+        {parentId: 'grass', value: 'bulbasaur-0', viewValue: 'Bulbasaur'},
+        {parentId: 'grass', value: 'oddish-1', viewValue: 'Oddish'},
+        {parentId: 'grass', value: 'bellsprout-2', viewValue: 'Bellsprout'}
+      ]
+    },
+    {
+      id: 'water',
+      name: 'Water',
+      pokemon: [
+        {parentId: 'water', value: 'squirtle-3', viewValue: 'Squirtle'},
+        {parentId: 'water', value: 'psyduck-4', viewValue: 'Psyduck'},
+        {parentId: 'water', value: 'horsea-5', viewValue: 'Horsea'}
+      ]
+    },
+    {
+      id: 'fire',
+      name: 'Fire',
+      disabled: true,
+      pokemon: [
+        {parentId: 'fire', value: 'charmander-6', viewValue: 'Charmander'},
+        {parentId: 'fire', value: 'vulpix-7', viewValue: 'Vulpix'},
+        {parentId: 'fire', value: 'flareon-8', viewValue: 'Flareon'}
+      ]
+    },
+    {
+      id: 'psychic',
+      name: 'Psychic',
+      pokemon: [
+        {parentId: 'psychic', value: 'mew-9', viewValue: 'Mew'},
+        {parentId: 'psychic', value: 'mewtwo-10', viewValue: 'Mewtwo'}
+      ]
+    }
+  ];
+
+  filteredOptions: any[];
+
+  ngOnInit() {
+    this.pokemonGroups.map((group) => {
+      this.groupControl.addControl(group.id, new FormControl());
+      this.intermediate[group.id] = false;
+    });
+
+    this.filteredOptions = [...this.pokemonGroups];
+
+    this.searchControl.valueChanges.subscribe((nextValue) => {
+      if (!!nextValue) {
+        this.filteredOptions = [...this.pokemonGroups].map((group) => ({
+          ...group,
+          pokemon: group.pokemon.filter((pokemon) =>
+            pokemon.viewValue.toLowerCase().indexOf(nextValue.toLowerCase()) !== -1)
+        }));
+        this.filteredOptions = [...this.filteredOptions].filter(group => group.pokemon.length > 0);
+      } else {
+        this.filteredOptions = [...this.pokemonGroups];
+      }
+    });
+
+    this.selectControl.valueChanges.subscribe(nextSelection => {
+      if (!!nextSelection) {
+        this.pokemonGroups.map((group) => {
+          this.intermediate[group.id] = false;
+        });
+        nextSelection.map((pokemon) => {
+          this.intermediate[pokemon.parentId] = true;
+        });
+      }
+    });
+
+    this.groupControl.valueChanges.pipe(
+      filter(() => !!this.selectControl.value)
+    ).subscribe(nextSelection => {
+      this.selectControl.setValue(this.selectControl.value.filter((pokemon) =>
+        nextSelection[pokemon.parentId]));
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatRippleModule} from '@angular/material/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatInputModule,
+    MatRippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
